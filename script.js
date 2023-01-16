003b0a89fc87e1c34510fb8cf4446a85629a1528
Igorbu123 <igorbulka123@gmail.com>
2023-01-16 17:51:52 +0100

    added RNG for computer choice

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 
+function RandomNum(min, max){
 
+    min = Math.ceil(min);
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function GetComputerChoice(num){
+
+}
+
+console.log(RandomNum(1, 3));
