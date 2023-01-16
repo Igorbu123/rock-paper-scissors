68f7cbed7ced8a90a7bbe64d498dd47fbd2d0957
Igorbu123 <igorbulka123@gmail.com>
2023-01-16 18:06:23 +0100

    added CPU choice function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+
 function RandomNum(min, max){
 
     min = Math.ceil(min);
@@ -7,8 +9,29 @@ function RandomNum(min, max){
 }
 
 
-function GetComputerChoice(num){
 
+function GetComputerChoice(CPU_choice){
+    
+    CPU_choice = RandomNum(1, 3);
+    
+    if(CPU_choice == 1){
+        console.log("Computer choose Rock!!")
+        CPU_choice = "Rock";
+        return CPU_choice;   
+    }
+    
+    else if(CPU_choice == 2){
+        console.log("computer choose Paper!!")
+        CPU_choice = "Paper";
+        return CPU_choice;   
+    }
+
+    else if(CPU_choice == 3){
+        console.log("computer choose Scissors!!")
+        CPU_choice = "Scissors";
+        return CPU_choice;  
+    }
+
+    
 }
-
-console.log(RandomNum(1, 3));
+GetComputerChoice();
