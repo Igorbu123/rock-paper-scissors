03c4cd5fc843127a7f9d519c21f0d3e160b6e0e3
Igorbu123 <igorbulka123@gmail.com>
2023-01-19 19:16:33 +0100

    connect game to UI and added 5 game limit and reset

--- script.js
+++ script.js
@@ -11,8 +11,31 @@ function RandomNum(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const btn_rock = document.querySelector('.btn_rock');
+const btn_papper = document.querySelector('.btn_papper');
+const btn_scissors = document.querySelector('.btn_scissors');
+const table1 = document.querySelector('.table1');
+const listElement1 = document.querySelector('.listElement1');
 
 
+btn_rock.addEventListener('click', () =>{
+    
+    choice_adjusted = "rock";
+    console.log(choice_adjusted);
+    
+});
+
+btn_papper.addEventListener('click', () =>{
+
+    choice_adjusted = "papper";
+    console.log(choice_adjusted);
+});
+
+btn_scissors.addEventListener('click', () =>{
+
+    choice_adjusted = "scissors";
+    console.log(choice_adjusted);
+})
 
 
 
@@ -52,30 +75,35 @@ function GetComputerChoice(CPU_choice){
 
 function PlayerSelection(PlayerChoice){
     
-    let choice = prompt("Rock, Papper, Scissors")
-    choice_adjusted = choice.toLowerCase();
+    
+
+    btn_rock.addEventListener('click', () =>{
+    
+        choice_adjusted = "rock";
         
-   
-    if(choice_adjusted == "rock"){
-    console.log("You chose Rock!!");
-    return choice_adjusted;
-    }
+        return PlayRound();
+        
+        
+    });
+    
+    btn_papper.addEventListener('click', () =>{
+        
+        choice_adjusted = "papper";
+        
+        PlayRound();
+    });
+    
+    btn_scissors.addEventListener('click', () =>{
 
-    else if(choice_adjusted == "papper"){
-    console.log("You chose Papper!!");
-    return choice_adjusted;
-    } 
+        choice_adjusted = "scissors";
+        
+        PlayRound();
 
-    else if(choice_adjusted == "scissors"){
-    console.log("You chose Scissors!!")
-    return choice_adjusted;
-    }
+    });  
 
-    else if(choice_adjusted != "rock" || "papper" || "scissors"){
-    console.log("Please input valid input");
-    }
+    
 }
-//PlayerSelection();
+PlayerSelection();
 //console.log(choice_adjusted);
 
 
@@ -86,73 +114,76 @@ function PlayerSelection(PlayerChoice){
 
 function PlayRound(cpupick,playerpick){
 
-   
+    
 
-    PlayerSelection();
     GetComputerChoice();
 
 
-    if(CPU_choice_adjusted == "rock" && choice_adjusted == "rock"){
-        console.log("It's a DRAW!!");
-    }   
-    else if(CPU_choice_adjusted == "rock" && choice_adjusted == "scissors"){
-        console.log("You LOSE!!");
-        return CPU_counter++;
+    if(PlayerCounter == 5){
+        console.log("YOU WIN!!!!!!!!!!!!!!!!!!!!")
+        PlayerCounter = 0;
+        CPU_counter = 0;
     }
-    else if(CPU_choice_adjusted == "rock" && choice_adjusted == "papper"){
-        console.log("You WIN!!");
-        return PlayerCounter++;
-    }
+        else if(CPU_counter == 5){
+            console.log("YOU LOSE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            PlayerCounter = 0;
+            CPU_counter = 0;
+        }
+
+    while(CPU_counter < 5 || PlayerCounter < 5){
+
+            console.log(CPU_counter);
+            console.log(PlayerCounter);
+    
+        if(CPU_choice_adjusted == "rock" && choice_adjusted == "rock"){
+            console.log("It's a DRAW!!");
+            return 0;
+        }   
+        else if(CPU_choice_adjusted == "rock" && choice_adjusted == "scissors"){
+            console.log("You LOSE!!");
+            return CPU_counter++;
+        }
+        else if(CPU_choice_adjusted == "rock" && choice_adjusted == "papper"){
+            console.log("You WIN!!");
+            return PlayerCounter++;
+        }
     
     
-    else if(CPU_choice_adjusted == "papper" && choice_adjusted == "papper"){
-        console.log("It's a DRAW!!");
-    }
-    else if(CPU_choice_adjusted == "papper" && choice_adjusted == "rock"){
-        console.log("You LOSE!!");
-        return CPU_counter++;
-    }
-    else if(CPU_choice_adjusted == "papper" && choice_adjusted == "scissors"){
-        console.log("You WIN!!");
-        return PlayerCounter++;
-    }
+        else if(CPU_choice_adjusted == "papper" && choice_adjusted == "papper"){
+            console.log("It's a DRAW!!");
+            return 0;
+        }
+        else if(CPU_choice_adjusted == "papper" && choice_adjusted == "rock"){
+            console.log("You LOSE!!");
+            return CPU_counter++;
+        }
+        else if(CPU_choice_adjusted == "papper" && choice_adjusted == "scissors"){
+            console.log("You WIN!!");
+            return PlayerCounter++;
+        }
 
 
-    else if(CPU_choice_adjusted == "scissors" && choice_adjusted == "scissors"){
-        console.log("It's a DRAW!!");
-    }
-    else if(CPU_choice_adjusted == "scissors" && choice_adjusted == "papper"){
-        console.log("you LOSE!!");
-        return CPU_counter++;
-    }
-    else if(CPU_choice_adjusted == "scissors" && choice_adjusted == "rock"){
-        console.log("You WIN!!");
-        return PlayerCounter++;
-    }
-}
-
-
-
-
-//plays a game of 5 rounds and declares winner
-
-function game(){
-
-
-    for(let i = 0; i < 5; i++){
-        
-        PlayRound();
-        
-    }
-
-    if(CPU_counter > PlayerCounter){
-        console.log("YOU DIED GIT GUT")
-    }
-    else if(PlayerCounter > CPU_counter){
-        console.log("YOU WIN GG EZ")
+        else if(CPU_choice_adjusted == "scissors" && choice_adjusted == "scissors"){
+            console.log("It's a DRAW!!");
+            return 0;
+        }
+        else if(CPU_choice_adjusted == "scissors" && choice_adjusted == "papper"){
+            console.log("you LOSE!!");
+            return CPU_counter++;
+        }
+        else if(CPU_choice_adjusted == "scissors" && choice_adjusted == "rock"){
+            console.log("You WIN!!");
+            return PlayerCounter++;
+        }   
     }
 }
-game();
+   
+
+
+
+
+
+
 
 
 //PlayRound();
